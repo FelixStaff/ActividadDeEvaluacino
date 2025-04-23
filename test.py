6c6ddd82deacebf1df4238726085d9632fe57728
Felipe de Jesús Félix Arredondo <a00833150@tec.mx>
2025-04-23 17:58:54 -0600

    k

--- test.py
+++ test.py
@@ -10,15 +10,5 @@ if __name__ == "__main__":
     # Load the model from the file
     with open('model.pkl', 'rb') as file:
         model = pickle.load(file)
-
-    # Example input data
-    input_data = {
-        'NameStyle': ['Missing'],
-        'Title': ['Missing'],
-        'FirstName': ['John'],
-        'MiddleName': ['Missing'],
-        'LastName': ['Doe'],
-        'Suffix': ['Missing'],
-        'CompanyName': ['Missing'],
-        'SalesPerson': ['Missing'],
-        'EmailAddress': ['
+    
+    #
